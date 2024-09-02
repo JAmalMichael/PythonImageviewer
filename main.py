f7abdff71254953a40cbe10e047ea884b9dcd86d
JAmalMichael <eshimi090@gmail.com>
2024-09-02 08:38:53 -1200

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+
+root = Tk()
+
+root.title("Image viewer")
+root.geometry("400x450")
+root.mainloop()
