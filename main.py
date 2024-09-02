b18cfa5da4a0d8b4cd4944d6d3bb0abb13831bd6
JAmalMichael <eshimi090@gmail.com>
2024-09-02 09:11:32 -1200

    showImage function

--- main.py
+++ main.py
@@ -4,7 +4,29 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import os
 
+
+def showImage():
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                          title="Select Image File", filetypes=(("JPG File", "*.jpg"), ("PNG File", "*.png"), ("All File", "*.*")))
+    img=Image.open(filename)
+    img=ImageTk.PhotoImage(img)
+    label.configure(image=img)
+    label.image=img
+
+
 root = Tk()
+
+frame = Frame(root)
+frame.pack(side=BOTTOM, padx=15, pady=15)
+
+label = Label(root)
+label.pack()
+
+btn1=Button(frame, text="Select-Image", command=showImage)
+btn1.pack(side=tk.LEFT)
+
+btn2=Button(frame, text="Exit", command=lambda:exit())
+btn2.pack(side=tk.LEFT, padx=12)
 
 root.title("Image viewer")
 root.geometry("400x450")
